6ccaebc89c40613449ac299640b0abd07c46356d
spacewander <spacewanderlzx@gmail.com>
2014-03-17 22:16:12 +0800

    fix

--- git.sh
+++ git.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 cd `pwd`
-if [[ $# > 1 && -d $1 ]]; # if the input is a directory, use git in it; or use git in pwd
+# if the input is a directory, use git in it; or use git in pwd
+if [[ $# > 0 && -d $1 ]]; 
 then
     cd $1
-    echo ''
 fi
 
-echo `pwd`
 git status
 echo 'Press RETURN to continue or use CTRL-C to leave'
 read # stop here and read the RETURN
 
-read -p "Enter your commit : " Message
-while [ -z $Message ]; do
-    read -p "Enter your commit : " $Message
+read -p "Enter your commit : " commit
+while [ -z "$commit" ]; do
+    read -p "Enter your commit : " commit
 done
 
-git add *
-git commit -m $Message
-echo ''
+git add -A
+git commit -m "$commit"
+echo '' # echo an empty line
 echo 'the commit will be push to origin/master immediately'
 echo 'Press RETURN to continue or use CTRL-C to leave'
 read # stop here and read the RETURN
