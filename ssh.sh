6bd5f588e7e6f67e6d319a21f881b9650423d53c
spacewander <spacewanderlzx@gmail.com>
2014-04-24 19:39:30 +0800

    添加了免输入密码ssh登录的脚本

---
+++ ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+auto_login_ssh () {
+    expect -c "set timeout -1;
+                spawn -noecho ssh -o StrictHostKeyChecking=no $2 ${@:3};
+                expect *assword:*;
+                send -- $1\r;
+                interact;";
+} 
+auto_login_ssh $password '-o ServerAliveInterval=60 $account'
+
