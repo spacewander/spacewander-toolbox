799da539e22b49d3825ab077760f6b5dcd90df5a
spacewander <spacewanderlzx@gmail.com>
2015-03-25 00:15:35 +0800

    重构了alias脚本，现在仅添加zsh的别名，且增加直接编辑别名文件的命令行参数

--- alias.sh
+++ alias.sh
@@ -1,17 +1,41 @@
-#!/bin/bash
+#!/bin/zsh
 
-# 同时向.zsh_aliases和.bash_aliases添加别名
+# 向.zsh_aliases添加别名，或用你喜欢的编辑器修改alias文件
 # 用法： alias.sh name alias_name , 
 # 比如 alias.sh git.sh gi 或 alias.sh 'rake test' rt
 
-if [ ! $# -eq 2 ]
-then
-    echo "用法： alia.sh name alias_name, 比如 alias.sh git.sh git"
-    exit 1
-fi
-
 zsh_aliases="$HOME/.zsh_aliases"
 bash_aliases="$HOME/.bash_aliases"
+
+if [ ! -e "$zsh_aliases" ]
+then
+    echo "$zsh_aliases 不存在！"
+fi
+if [ ! -e "$bash_aliases" ]
+then
+    echo "$bash_aliases 不存在！"
+fi
+
+if [ ! $# -eq 2 ]
+then
+    if [ $# -eq 1 ]
+    then
+        test -n "$EDITOR" && EDITOR=/etc/alternatives/editor
+        case $1 in
+            -b )
+                "$EDITOR" "$bash_aliases"
+                source "$bash_aliases"
+                ;;
+            -z )
+                "$EDITOR" "$zsh_aliases"
+                source "$zsh_aliases"
+                ;;
+            * )
+                echo "用法： alia.sh name alias_name, 比如 alias.sh git.sh git"
+        esac
+    fi
+    exit 0
+fi
 
 path=$1
 
@@ -23,30 +47,23 @@ fi
 
 cmd=$2
 
-if [ -e "$zsh_aliases" ]
+if [[ $path =~ \' ]]
 then
-    if [[ $path =~ \' ]]
-    then
-        echo "alias $cmd="'"'"$path"'"' >> "$zsh_aliases"
-    else
-        echo "alias $cmd='$path'" >> "$zsh_aliases"
-    fi
-    # bash的source不兼容zsh的alias命令，所以无法source。
-    # 如果改成#!..zsh的话，又不能使用basename……
-    #source "$zsh_aliases"
+    echo "alias $cmd="'"'"$path"'"' >> "$zsh_aliases"
 else
-    echo "$zsh_aliases 不存在！"
+    echo "alias $cmd='$path'" >> "$zsh_aliases"
 fi
+source "$zsh_aliases"
 
-if [ -e  "$bash_aliases" ]
-then
-    if [[ $path =~ \' ]]
-    then
-        echo "alias $cmd="'"'"$path"'"' >> "$bash_aliases"
-    else
-        echo "alias $cmd='$path'" >> "$bash_aliases"
-    fi
-    source "$bash_aliases"
-else
-    echo "$bash_aliases 不存在！"
-fi
+#if [ -e  "$bash_aliases" ]
+#then
+    #if [[ $path =~ \' ]]
+    #then
+        #echo "alias $cmd="'"'"$path"'"' >> "$bash_aliases"
+    #else
+        #echo "alias $cmd='$path'" >> "$bash_aliases"
+    #fi
+    #source "$bash_aliases"
+#else
+    #echo "$bash_aliases 不存在！"
+#fi
